6adac52f51fde1f22eacca06b3ea7101bf181a56
Artem14072003 <artem_krivenko_2019@mail.ru>
2024-03-14 17:06:15 +0300

    Ready Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: 'Главная',
+  description: '',
+};
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <Hero />
+    </main>
   );
 }
